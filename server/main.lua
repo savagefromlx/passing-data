345c25d2c77ee38a93556be9c97e265cb25c9a3e
SavageFromLx <72369074+savagefromlx@users.noreply.github.com>
2021-10-10 21:50:29 +0100

    updating code

--- server/main.lua
+++ server/main.lua
@@ -1,4 +1,8 @@
 RegisterCommand('gotoclient', function(source, args, rawCommand) 
+	if (source == 0 or source == nil) then 
+		return print('Command only allowed in game.') -- This is console, we don't wan't to use this on console, the code will stop running from here. We are using "return" to stop code.
+	end
+
     local value = 'Hello client-side'
     TriggerClientEvent('SendToClient', source, value)
     
